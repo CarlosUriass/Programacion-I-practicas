a1116a4fd0c3c439331f0950fb4a7fda2e8413b4
unknown <hicarlosurias@gmail.com>
2024-03-25 14:11:24 -0700

    Practica 13

---
+++ Practicas/13/Utilidades/Utilidades/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        txtb_nombre = New TextBox()
+        txtb_dias = New TextBox()
+        Label3 = New Label()
+        txtb_sueldo = New TextBox()
+        Label4 = New Label()
+        txtb_cuenta = New TextBox()
+        Label5 = New Label()
+        txtb_email = New TextBox()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label6 = New Label()
+        txtb_utilidad = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(179, 128)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Nombre:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(373, 128)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(90, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Dias trabajados:"
+        ' 
+        ' txtb_nombre
+        ' 
+        txtb_nombre.Location = New Point(239, 128)
+        txtb_nombre.Name = "txtb_nombre"
+        txtb_nombre.Size = New Size(105, 23)
+        txtb_nombre.TabIndex = 2
+        ' 
+        ' txtb_dias
+        ' 
+        txtb_dias.Location = New Point(469, 125)
+        txtb_dias.Name = "txtb_dias"
+        txtb_dias.Size = New Size(105, 23)
+        txtb_dias.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(146, 176)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(87, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Sueldo (diario):"
+        ' 
+        ' txtb_sueldo
+        ' 
+        txtb_sueldo.Location = New Point(239, 168)
+        txtb_sueldo.Name = "txtb_sueldo"
+        txtb_sueldo.Size = New Size(105, 23)
+        txtb_sueldo.TabIndex = 5
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(370, 176)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(93, 15)
+        Label4.TabIndex = 6
+        Label4.Text = "Cuenta bancaria"
+        ' 
+        ' txtb_cuenta
+        ' 
+        txtb_cuenta.Location = New Point(469, 173)
+        txtb_cuenta.Name = "txtb_cuenta"
+        txtb_cuenta.Size = New Size(105, 23)
+        txtb_cuenta.TabIndex = 7
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(194, 210)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(39, 15)
+        Label5.TabIndex = 8
+        Label5.Text = "Email:"
+        ' 
+        ' txtb_email
+        ' 
+        txtb_email.Location = New Point(239, 202)
+        txtb_email.Name = "txtb_email"
+        txtb_email.Size = New Size(335, 23)
+        txtb_email.TabIndex = 9
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(-2, 293)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(803, 94)
+        ListBox1.TabIndex = 10
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(338, 231)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(114, 23)
+        Button1.TabIndex = 11
+        Button1.Text = "Siguiente"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(674, 393)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(114, 23)
+        Button2.TabIndex = 12
+        Button2.Text = "Calcular utilidades"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(179, 40)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(101, 15)
+        Label6.TabIndex = 13
+        Label6.Text = "Utilidad a repartir:"
+        ' 
+        ' txtb_utilidad
+        ' 
+        txtb_utilidad.Location = New Point(286, 37)
+        txtb_utilidad.Name = "txtb_utilidad"
+        txtb_utilidad.Size = New Size(166, 23)
+        txtb_utilidad.TabIndex = 14
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtb_utilidad)
+        Controls.Add(Label6)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(txtb_email)
+        Controls.Add(Label5)
+        Controls.Add(txtb_cuenta)
+        Controls.Add(Label4)
+        Controls.Add(txtb_sueldo)
+        Controls.Add(Label3)
+        Controls.Add(txtb_dias)
+        Controls.Add(txtb_nombre)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtb_nombre As TextBox
+    Friend WithEvents txtb_dias As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtb_sueldo As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtb_cuenta As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtb_email As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtb_utilidad As TextBox
+
+End Class
---
+++ Practicas/13/Utilidades/Utilidades/Form1.vb
@@ -0,0 +1,278 @@
+﻿Public Class Form1
+    Dim nombre As String
+    Dim dias As String
+    Dim email As String
+    Dim sueldo As String
+    Dim cuenta As String
+    Dim matriz(20, 4) As String 'MATRIZ SOLO PARA 20 EMPLEADOS
+    Dim contador As Byte = 0
+    Dim utilidad As Double
+    Dim importe As Double
+    Dim porcentaje As Double
+    Dim utilidad_pagar As Double
+    Dim Calculo_sueldo, suma_sueldo As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        nombre = txtb_nombre.Text
+
+        'Validar Nombre
+
+        If nombre = "" Then
+            MsgBox("El nombre no puede estar vacio", MsgBoxStyle.OkOnly)
+        End If
+
+        'Validar que el nombre no tenga numeros
+        For i As Integer = 1 To Len(nombre)
+            If nombre(i - 1) = "0" Or nombre(i - 1) = "1" Or nombre(i - 1) = "2" Or nombre(i - 1) = "3" Or nombre(i - 1) = "4" Or nombre(i - 1) = "5" Or nombre(i - 1) = "6" Or nombre(i - 1) = "7" Or nombre(i - 1) = "8" Or nombre(i - 1) = "9" Then
+                MsgBox("El nombre no puede contener números", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+        'Validar que el nombre no contenga caracteres especiales
+
+        For i As Integer = 1 To Len(nombre)
+            If nombre(i - 1) = "#" Or nombre(i - 1) = "/" Or nombre(i - 1) = "!" Or nombre(i - 1) = "$" Or nombre(i - 1) = "%" Or nombre(i - 1) = "&" Or nombre(i - 1) = "*" Or nombre(i - 1) = "+" Or nombre(i - 1) = "=" Or nombre(i - 1) = "?" Or nombre(i - 1) = "^" Or nombre(i - 1) = "`" Or nombre(i - 1) = "|" Or nombre(i - 1) = "~" Then
+                MsgBox("No se admiten caracteres especiales en el nombre", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+
+        'VALIDAR DIAS TRABAJADOS
+
+        'Validar que los dias trabajados no esten vacios
+
+        If txtb_dias.Text = "" Then
+            MsgBox("El campo dias trabajados no puede estar vacio", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+        ' Validar que solo sean números
+        For i As Integer = 1 To Len(txtb_dias.Text)
+            If Not Char.IsDigit(txtb_dias.Text(i - 1)) Then
+                MsgBox("Los días deben contener solo dígitos del 0 al 9", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+        ' Validar que no existan caracteres especiales
+        For i As Integer = 1 To Len(txtb_dias.Text)
+            If txtb_dias.Text(i - 1) = "#" Or txtb_dias.Text(i - 1) = "/" Or txtb_dias.Text(i - 1) = "!" Or txtb_dias.Text(i - 1) = "$" Or txtb_dias.Text(i - 1) = "%" Or txtb_dias.Text(i - 1) = "&" Or txtb_dias.Text(i - 1) = "*" Or txtb_dias.Text(i - 1) = "+" Or txtb_dias.Text(i - 1) = "=" Or txtb_dias.Text(i - 1) = "?" Or txtb_dias.Text(i - 1) = "^" Or txtb_dias.Text(i - 1) = "`" Or txtb_dias.Text(i - 1) = "|" Or txtb_dias.Text(i - 1) = "~" Then
+                MsgBox("Los días no pueden contener caracteres especiales", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+        'Validar que los dias no sean menores a 1
+        If txtb_dias.Text < "1" Then
+            MsgBox("Los dias trabajados no pueden ser menores a 1")
+            Return
+        End If
+
+
+        'VALIDAR SUELDO
+
+
+        'Validar que el sueldo diario no esté vacio
+        If txtb_sueldo.Text = "" Then
+            MsgBox("El campo sueldo no puede estar vacio", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+
+
+        'Validar que no existan caracteres especiales
+        For i As Integer = 1 To Len(txtb_sueldo.Text)
+            If txtb_sueldo.Text(i - 1) = "#" Or txtb_sueldo.Text(i - 1) = "/" Or txtb_sueldo.Text(i - 1) = "!" Or txtb_sueldo.Text(i - 1) = "$" Or txtb_sueldo.Text(i - 1) = "%" Or txtb_sueldo.Text(i - 1) = "&" Or txtb_sueldo.Text(i - 1) = "*" Or txtb_sueldo.Text(i - 1) = "+" Or txtb_sueldo.Text(i - 1) = "=" Or txtb_sueldo.Text(i - 1) = "?" Or txtb_sueldo.Text(i - 1) = "^" Or txtb_sueldo.Text(i - 1) = "`" Or txtb_sueldo.Text(i - 1) = "|" Or txtb_sueldo.Text(i - 1) = "~" Then
+                MsgBox("El sueldo no puede contener caracteres especiales", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+
+        'Validar que el sueldo sea mayor que 0
+        If txtb_sueldo.Text < "1" Then
+            MsgBox("El sueldo diario no puede ser menor a 0", MsgBoxStyle.OkOnly)
+            Exit Sub
+            Return
+        End If
+
+
+        'VALIDAR CUENTA BANCARIA
+
+        'Valiudar cuenta bancaria
+
+
+        If txtb_cuenta.Text = "" Then
+            MsgBox("la cuenta bancaria puede estar vacia", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+        'Validar que la cuenta bancaria no contenga caracteres especiales
+
+        For i As Integer = 1 To Len(txtb_cuenta.Text)
+            If txtb_cuenta.Text(i - 1) = "#" Or txtb_cuenta.Text(i - 1) = "/" Or txtb_cuenta.Text(i - 1) = "!" Or txtb_cuenta.Text(i - 1) = "$" Or txtb_cuenta.Text(i - 1) = "%" Or txtb_cuenta.Text(i - 1) = "&" Or txtb_cuenta.Text(i - 1) = "*" Or txtb_cuenta.Text(i - 1) = "+" Or txtb_cuenta.Text(i - 1) = "=" Or txtb_cuenta.Text(i - 1) = "?" Or txtb_cuenta.Text(i - 1) = "^" Or txtb_cuenta.Text(i - 1) = "`" Or txtb_cuenta.Text(i - 1) = "|" Or txtb_cuenta.Text(i - 1) = "~" Then
+                MsgBox("No se admiten caracteres especiales en la cuenta bancaria", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+        ' Validar que la cuenta bancaria solo contenga números (No letras)
+        For i As Integer = 1 To Len(txtb_cuenta.Text)
+            If Not Char.IsDigit(txtb_cuenta.Text(i - 1)) Then
+                MsgBox("La cuenta bancaria no puede contener caracteres diferentes de los dígitos del 0 al 9", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+        ' Validar longitud de la cuenta bancaria
+        If Len(txtb_cuenta.Text) <> 8 Then
+            MsgBox("La cuenta bancaria debe tener una longitud de 8 caracteres", MsgBoxStyle.OkOnly)
+            Exit Sub
+            Return
+        End If
+
+        'VALIDAR EMAIL
+
+        email = LCase(txtb_email.Text)
+
+        If email = "" Then
+            MsgBox("El email no puede estar vacío", MsgBoxStyle.OkOnly)
+            Exit Sub
+        End If
+
+        'Buscar arrobas
+        Dim arroba_posc As Integer
+        Dim contador_arroba As Integer
+        contador_arroba = 0
+        For i As Integer = 1 To Len(email)
+            If email(i - 1) = "@" Then
+                arroba_posc = i
+                contador_arroba += 1
+            End If
+        Next
+
+        If contador_arroba > 1 Then
+            MsgBox("Solo debe existir una arroba en el email", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+        If contador_arroba = 0 Then
+            MsgBox("El correo debe contener al menos un arroba", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+        'Generar nombre de usuario
+        Dim nombre_usuario As String
+        nombre_usuario = Microsoft.VisualBasic.Left(email, arroba_posc - 1)
+
+        'Validar si el nombre de usuario excede los 64 caracteres
+        If Len(nombre_usuario) > 64 Then
+            MsgBox("El nombre de usuario excede los 64 caracteres", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+        'Generar dominio
+        Dim dominio As String
+        dominio = Mid(email, arroba_posc + 1)
+
+        If Len(dominio) >= 255 Then
+            MsgBox("Los caracteres en el dominio exceden el límite permitido", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+
+        For i As Integer = 1 To Len(email)
+            If email(i - 1) = "#" Or email(i - 1) = "/" Or email(i - 1) = "!" Or email(i - 1) = "$" Or email(i - 1) = "%" Or email(i - 1) = "&" Or email(i - 1) = "*" Or email(i - 1) = "+" Or email(i - 1) = "=" Or email(i - 1) = "?" Or email(i - 1) = "^" Or email(i - 1) = "`" Or email(i - 1) = "|" Or email(i - 1) = "~" Then
+                MsgBox("No se admiten caracteres especiales", MsgBoxStyle.OkOnly)
+                Exit For
+                Return
+            End If
+        Next
+
+
+        If Not (dominio.EndsWith(".com") Or dominio.EndsWith(".gob") Or dominio.EndsWith(".org") Or dominio.EndsWith(".net")) Then
+            MsgBox("El dominio del correo electrónico no es válido.", MsgBoxStyle.OkOnly)
+            Return
+        End If
+
+
+
+        'En teoria si llega hasta aqui es que estan todos los campos validados por la función return
+        nombre = txtb_nombre.Text
+        dias = txtb_dias.Text
+        sueldo = txtb_sueldo.Text
+        cuenta = txtb_cuenta.Text
+        email = txtb_email.Text
+
+        'Llenar esos datos a la matriz
+        matriz(contador, 0) = nombre
+        matriz(contador, 1) = dias
+        matriz(contador, 2) = sueldo
+        matriz(contador, 3) = cuenta
+        matriz(contador, 4) = email
+
+
+
+        'Ir sumando el total de los sueldos
+
+        If Val(matriz(contador, 1)) >= 60 Then
+            Calculo_sueldo = Val(matriz(contador, 1) * Val(matriz(contador, 2)))
+            suma_sueldo += Calculo_sueldo
+        End If
+
+
+        'Sumar 1 al contador
+
+        contador += 1
+
+
+
+        'Limpiar casillas 
+        'De nuevo: En teoria, si la logica del programa llega hasta la ejecución de esta linea se supone que todo estuvo validado
+        txtb_nombre.Clear()
+        txtb_dias.Clear()
+        txtb_sueldo.Clear()
+        txtb_cuenta.Clear()
+        txtb_email.Clear()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        utilidad = Val(txtb_utilidad.Text)
+
+
+        For i As Integer = 0 To 20 'Recorrer filas
+            If matriz(i, 1) >= 60 Then
+
+                importe = Val(matriz(i, 1)) * Val(matriz(i, 2))
+
+                'Calcular el porcentaje relativo 
+                porcentaje = Val((importe / suma_sueldo) / 1)
+
+                'Calcular utilidad 
+
+                utilidad_pagar = porcentaje * utilidad
+
+                'Imprimir resultados en listbox
+                ListBox1.Items.Add("Nombre: " & matriz(i, 0) & " Utilidades a pagar: " & Math.Round(utilidad_pagar, 2) & " Cuenta bancaria: " & matriz(i, 3) & " Email: " & matriz(i, 4))
+            End If
+
+        Next
+
+    End Sub
+End Class
+
+
+'Notas:
+''' Habria de validar que el importe de las utilidades a repartir no se pueda modificar
+''' Un mensaje que avise que una vez de calcular los datos podria tener alteraciones si no estan completos
+''' (Confirmación)
+'''
